ed62e9ec65ef9f95c7a8f7669da7dd5dddc39411
Boris Waguia <bwa@adorsys.de>
2020-02-28 08:21:59 +0100

    Add parseAndCreateSha documentation

--- jcrypt-cli-shasum/src/main/java/com/jcryptcli/shasum/util/SHaUtil.java
+++ jcrypt-cli-shasum/src/main/java/com/jcryptcli/shasum/util/SHaUtil.java
@@ -11,6 +11,12 @@ public class SHaUtil {
 
     public SHaUtil () {}
 
+    /**
+     * Parse command line arguments and compute the SHA value of the provided text.
+     * @param args
+     * @param parser
+     * @return
+     */
     public Map<String, String> parseAndCreateSha(String[] args, SupportedArgumentParser parser) {
         Arguments arguments = new ArgumentBuilder(args, parser).getAsArguments();
         ArgumentProcessor processor = ShaArgumentProcessorFactory.getInstance();
